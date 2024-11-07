cd7950c3ae97fedcc7311bf528b25f0aedbf359e
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-07 09:39:10 +0000

    feat: delete state column
    No need for this column as it is identical to the rownames

--- Rscripts/emission_similarity.R
+++ Rscripts/emission_similarity.R
@@ -3,8 +3,8 @@ emission_file_one <- args[[1]]
 emission_file_two <- args[[2]]
 output_file <- args[[3]]
 
-rename_state_column <- function(emissions_table) {
-  return(dplyr::rename(emissions_table, State = "State (Emission order)"))
+remove_state_column <- function(emissions_table) {
+  return(dplyr::select(emissions_table, -"State (Emission order)"))
 }
 
 match_columns <- function(table_one, table_two) {
@@ -17,8 +17,8 @@ main <- function(emission_file_one, emission_file_two, output_file) {
   emissions_one <- data.table::fread(emission_file_one)
   emissions_two <- data.table::fread(emission_file_two)
 
-  emissions_one <- rename_state_column(emissions_one)
-  emissions_two <- rename_state_column(emissions_two)
+  emissions_one <- remove_state_column(emissions_one)
+  emissions_two <- remove_state_column(emissions_two)
 
   matched_emissions <- match_columns(emissions_one, emissions_two)
   emissions_one <- matched_emissions[[1]]
