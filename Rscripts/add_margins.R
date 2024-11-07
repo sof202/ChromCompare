8fb05db5ba555023299c173d4bbd2c5caae7b3e5
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-07 14:57:13 +0000

    feat: add the margin adding function

--- Rscripts/add_margins.R
+++ Rscripts/add_margins.R
@@ -1,6 +1,11 @@
 main <- function(state_assignments_file, margin) {
   state_assignments <- data.table::fread(state_assignments_file)
   state_assignments <- add_margins(state_assignments)
+add_margins <- function(state_assignments, margin) {
+  state_assignments <- state_assignments |>
+    dplyr::mutate(start = start - margin, end = end + margin)
+}
+  state_assignments <- add_margins(state_assignments, margin)
   save_file(state_assignments)
 }
 
