75c0ad3456b275b5e86ec664bfc4ee6f35ecdb53
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-11 15:31:41 +0000

    feat: add function to add states column

--- Rscripts/spatial_similarity.R
+++ Rscripts/spatial_similarity.R
@@ -1,21 +1,20 @@
+generate_stats_table <- function(combined_assignments, column) {
+  states_present <- unique(combined_assignments[[column]])
+  sorted_states <- sort(states_present)
+  stats_table <- data.table::data.table("state" = sorted_states)
+  return(stats_table)
+}
+
 main <- function(combined_assignments_file, bin_size, output_file_path) {
   combined_assignments <- data.table::fread(
     combined_assignments_file,
     colnames = c("model_one", "model_two")
   )
-  model_one_stats_table <- data.table::data.table(
-    "state" = numeric(1),
-    "bp_in_assignment" = numeric(1),
-  )
-  model_two_stats_table <- data.table::data.table(
-    "state" = numeric(1),
-    "bp_in_assignment" = numeric(1),
-  )
-  model_one_stats_table <- model_one_stats_table |>
-    fill_in_states(combined_assignments, 1) |>
+  model_one_stats_table <-
+    generate_stats_table(combined_assignments, column = "model_one") |>
     add_bp_coverage(combined_assignments, bin_size, 1)
-  model_two_stats_table <- model_two_stats_table |>
-    fill_in_states(combined_assignments, 2) |>
+  model_two_stats_table <-
+    generate_stats_table(combined_assignments, column = "model_two") |>
     add_bp_coverage(combined_assignments, bin_size, 2)
   stats_table <- merge_stats_tables(
     model_one_stats_table,
