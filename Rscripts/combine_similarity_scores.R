e3f37e36cc247fb27ad1c48947c8f54910a5d70a
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-20 15:16:15 +0000

    feat: add functions to convert distances to scores

--- Rscripts/combine_similarity_scores.R
+++ Rscripts/combine_similarity_scores.R
@@ -8,17 +8,50 @@ read_matrix <- function(file_path) {
   return(matrix)
 }
 
+calculate_max_fold_enrichment <- function(spatial_similarities) {
+  max_fold_enrichments <- vapply(
+    spatial_similarities,
+    max,
+    numeric(1)
+  )
+  return(max(max_fold_enrichments))
+}
+
+calculate_emission_score <- function(distance,
+                                     max_distance,
+                                     max_fold_enrichment) {
+  max_fold_enrichment * (1 - distance / max_distance)
+}
+
+generate_emission_similarities <- function(emission_similarities,
+                                           max_fold_enrichment) {
+  max_euclidean_distance <- max(emission_similarities)
+  emission_similarity_scores <- apply(
+    emission_similarities,
+    c(1, 2),
+    calculate_emission_score,
+    max_euclidean_distance,
+    max_fold_enrichment
+  )
+  return(emission_similarity_scores)
+}
+
 
 main <- function(emission_similarities_file,
                  spatial_similarities_file_list,
                  weights,
                  output_file_path) {
-  emission_similarities <- read_matrix(emission_similarities_file)
+  emission_distances <- read_matrix(emission_similarities_file)
   spatial_similarities <- lapply(
     spatial_similarities_file_list,
     function(file_path) {
       read_matrix(file_path)
     }
+  )
+  max_fold_enrichment <- calculate_max_fold_enrichment(spatial_similarities)
+  emission_similarities <- generate_emission_similarities(
+    emission_distances,
+    max_fold_enrichment
   )
   all_similarity_matrices <- append(
     emission_similarities,
