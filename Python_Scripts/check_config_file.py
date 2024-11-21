11694b447e8b1d71ffe5779630b7ce3b4548dc54
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-21 10:15:23 +0000

    feat: add eol validation
    Config file must be in linux eol format as this is what the rest of the
    pipeline implicitly depends on. A different eol format would result in
    the `source` commands to fail.

--- Python_Scripts/check_config_file.py
+++ Python_Scripts/check_config_file.py
@@ -1,4 +1,25 @@
 import argparse
+import sys
+
+
+def validate_eol_format(file_path: str) -> None:
+    try:
+        with open(file_path, "rb") as config_file:
+            content: bytes = config_file.read()
+    except IOError:
+        print(
+            "Could not read file",
+            file_path,
+            "Please ensure that this file exists and has read permissions."
+        )
+        sys.exit(1)
+    if b'\r' in content:
+        print(
+            "Carriage return character found in file.",
+            "Please ensure that config file uses Linux EOL characters only."
+        )
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -7,7 +28,7 @@ if __name__ == "__main__":
     )
     parser.add_argument('file_path')
     args = parser.parse_args()
-    check_eol_type(args.file_path)
+    validate_eol_format(args.file_path)
     config_variables = get_config_variables(args.file_path)
     check_types(config_variables)
     check_file_paths(config_variables)
