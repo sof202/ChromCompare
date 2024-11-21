e9d4254d91548dc212718c0dd73a1b8b68c7f1ee
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-21 15:17:59 +0000

    feat: add function to reshape the data for plotting

--- Rscripts/generate_heatmap.R
+++ Rscripts/generate_heatmap.R
@@ -4,12 +4,25 @@ read_matrix <- function(file_path) {
     sep = ",",
     header = TRUE
   )
-  matrix <- as.matrix(matrix, rownames = seq_len(nrow(matrix)))
   return(matrix)
+}
+
+reshape_data <- function(data) {
+  # Data needs to be reshaped in order to be in the form that will work with
+  # ggplot
+  data <- dplyr::mutate(data, "model_two_state" = rownames(data), .before = 1)
+  data <- tidyr::pivot_longer(
+    data,
+    cols = -model_two_state,
+    names_to = "model_one_state",
+    values_to = "similarity_score"
+  )
+  return(data)
 }
 
 main <- function(matrix_file_path, output_file_path) {
   matrix <- read_matrix(matrix_file_path)
+  data <- reshape_data(matrix)
   heatmap <- generate_heatmap(matrix)
   ggsave(
     output_file_path,
